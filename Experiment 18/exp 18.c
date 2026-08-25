921cd966dde24208f7d23ff7bad101160b810fdc
mdeepika2006 <166002064+mdeepika2006@users.noreply.github.com>
2026-08-25 10:34:49 +0530

    Add files via upload

--- Experiment 18/exp 18.c
+++ Experiment 18/exp 18.c
@@ -1,1 +1,35 @@
+#include <reg51.h>
 
+sbit PWM = P1^0;
+
+unsigned char count = 0;
+unsigned char duty = 100;   // Change this value (0-200)
+
+void timer0_ISR(void) interrupt 1
+{
+    TH0 = 0xFC;
+    TL0 = 0x66;
+
+    count++;
+
+    if(count < duty)
+        PWM = 1;
+    else
+        PWM = 0;
+
+    if(count >= 200)
+        count = 0;
+}
+
+void main()
+{
+    TMOD = 0x01;   // Timer0 Mode 1 (16-bit)
+
+    TH0 = 0xFC;
+    TL0 = 0x66;
+
+    IE = 0x82;     // EA = 1, ET0 = 1
+    TR0 = 1;       // Start Timer0
+
+    while(1);
+}
